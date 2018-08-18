56fd91c08e421672a370a2c17dda7bbab5e92b62
Tim De Jong <tjadejong@thinkpractice.nl>
2018-08-18 13:26:07 +0200

    Refactored log_dir_for into ProjectPaths and added check_point_dir_for as well. More work on creating the possibility to run, evaluate and save multiple models in one go.

--- ProjectPaths.py
+++ ProjectPaths.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 
 class ProjectPaths(object):
@@ -20,3 +21,15 @@ class ProjectPaths(object):
     @classmethod
     def log_dir(cls):
         return os.path.join(ProjectPaths.base_dir(), "Logs")
+
+    @classmethod
+    def log_dir_for(cls, model_name, batch_size, epochs, lr):
+        date = str(datetime.now().date())
+        log_dir_name = model_name + "_" + date + "_" + str(batch_size) + "_" + str(epochs) + "_" + str(lr)
+        return os.path.join(ProjectPaths.log_dir(),  log_dir_name)
+
+    @classmethod
+    def checkpoint_dir_for(cls, model_name, batch_size, epochs):
+        date = str(datetime.now().date())
+        check_point_dir = model_name + "_" + date + "_" + str(batch_size) + "_" + str(epochs)
+        return os.path.join(ProjectPaths.model_dir(), check_point_dir)
--- TrainModel.py
+++ TrainModel.py
@@ -2,9 +2,8 @@ from ProjectPaths import ProjectPaths
 from PerformanceMetrics import PerformanceMetrics
 from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
-from keras.callbacks import TensorBoard
+from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.applications.vgg16 import VGG16
-from datetime import datetime
 import numpy as np
 import pandas as pd
 import os
@@ -25,48 +24,55 @@ print("- Validation-set:\t{}".format(len(valid_labels)))
 print("- Test-set:\t\t{}".format(len(test_labels)))
 
 def models():
-    return {"vgg16": ""}
+    return {"vgg16": lambda pre_trained_weights, include_top : VGG16(weights=pre_trained_weights, include_top=False) }
 
 
-def log_dir_for(model_name, batch_size, epochs, lr):
-    date = str(datetime.now().date())
-    log_dir_name = model_name + "_" + date + "_" + str(batch_size) + "_" + str(epochs) + "_" + str(lr)
-    return os.path.join(ProjectPaths.log_dir(),  log_dir_name)
+def base_model_for(model_name, pre_trained_weights = "imagenet", include_top=False):
+    #return VGG16(weights=pre_trained_weights, include_top=False)
+    return models()[model_name](pre_trained_weights, include_top)
 
-def base_model_for(model_name, pre_trained_weights = "imagenet"):
-    return VGG16(weights=pre_trained_weights, include_top=False)
-
-def compile_model(model_name):
+def compile_model(model_name, all_trainable=False):
     base_model = base_model_for(model_name)
     base_model.summary()
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
     x = Dense(1024, activation='relu')(x)
     predictions = Dense(1, activation='sigmoid')(x)
+
     model = Model(inputs=base_model.input, outputs=predictions)
     for layer in base_model.layers:
-        layer.trainable = False
+        layer.trainable = all_trainable
+
     model.compile(optimizer='rmsprop', loss='binary_crossentropy',
                   metrics=['accuracy', PerformanceMetrics.precision,
                            PerformanceMetrics.recall, PerformanceMetrics.fmeasure])
     return model
 
 def train_model(model_name, model, batch_size, epochs, train_images, train_labels, validation_images, validation_labels, verbose=True):
-    log_dir = log_dir_for(model_name, batch_size, epochs, 0)
+    checkpoint_dir = ProjectPaths.checkpoint_dir_for(model_name, batch_size, epochs)
+    model_checkpoint_callback = ModelCheckpoint(checkpoint_dir, monitor='val_acc', verbose= verbose, save_weights_only=True,
+                                          save_best_only=True, mode='max', period=1)
+
+    log_dir = ProjectPaths.log_dir_for(model_name, batch_size, epochs, 0)
     tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=0,  write_graph=False, write_images=False)
-    model.fit(x = train_images, y = train_labels, epochs = epochs, callbacks= [tensorboard_callback],
+
+    model.fit(x = train_images, y = train_labels, epochs = epochs, callbacks= [model_checkpoint_callback, tensorboard_callback],
               validation_data = (validation_images, validation_labels), batch_size = batch_size,
               verbose = verbose)
 
-def evaluate_model(model_name, model, train_images, train_labels, test_images, test_labels, validation_images, validation_labels):
-  train_acc = round(model.evaluate(train_images, train_labels, 128)[1],4)
-  valid_acc = round(model.evaluate(validation_images, validation_labels, 128)[1],4)
-  test_acc = round(model.evaluate(test_images, test_labels, 128)[1],4)
+def evaluate_model(model_name, model, batch_size, train_images, train_labels, test_images, test_labels, validation_images, validation_labels):
+    train_acc = round(model.evaluate(train_images, train_labels, batch_size)[1],4)
+    valid_acc = round(model.evaluate(validation_images, validation_labels, batch_size)[1],4)
+    test_acc = round(model.evaluate(test_images, test_labels, batch_size)[1],4)
 
-  print("- Training accuracy:\t{}".format(train_acc))
-  print("- Validation accuracy:\t{}".format(valid_acc))
-  print("- Test accuracy:\t{}".format(test_acc))
-  return model_name, train_acc, test_acc, valid_acc
+    print(model.metrics_names)
+    print("- Training accuracy:\t{}".format(train_acc))
+    print("- Validation accuracy:\t{}".format(valid_acc))
+    print("- Test accuracy:\t{}".format(test_acc))
+    return model_name, train_acc, test_acc, valid_acc
+
+def save_model(model, file_name):
+    pass
 
 batch_size = 64
 step_size = 420
@@ -80,7 +86,7 @@ model_evaluations = []
 for model_name in model_names:
     model = compile_model(model_name)
     train_model(model_name, model, batch_size, epochs, train_images, train_labels, valid_images, valid_labels)
-    model_evaluations.append(evaluate_model(model_name, model, train_images, train_labels, test_images, test_labels, valid_images, valid_labels))
+    model_evaluations.append(evaluate_model(model_name, model, batch_size, train_images, train_labels, test_images, test_labels, valid_images, valid_labels))
 
 evaluation_names = ["model_name", "train_acc", "test_acc", "valid_acc"]
 np_model_evaluations = np.array(model_evaluations)
