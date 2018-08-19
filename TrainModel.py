0becf8c02abe9a081dc9545dbe115e6e633af109
Tim De Jong <tjadejong@thinkpractice.nl>
2018-08-19 11:44:23 +0200

    Made optimizer configurable and changed RunSettings from dictionary to namedtuple.

--- TrainModel.py
+++ TrainModel.py
@@ -4,7 +4,9 @@ from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.applications.vgg16 import VGG16
+from keras.optimizers import SGD, RMSprop
 from sklearn.metrics import confusion_matrix
+from collections import namedtuple
 import numpy as np
 import pandas as pd
 
@@ -23,16 +25,23 @@ print("- Training-set:\t\t{}".format(len(train_labels)))
 print("- Validation-set:\t{}".format(len(valid_labels)))
 print("- Test-set:\t\t{}".format(len(test_labels)))
 
+RunSettings = namedtuple("RunSettings", ["model_name", "pre_trained_weights", "include_top", "all_trainable", "batch_size", "epochs", "optimizer", "lr", "momentum", "decay", "nesterov"])
 
 def models():
-    return {"vgg16": lambda pre_trained_weights, include_top : VGG16(weights=pre_trained_weights, include_top=False) }
+    return {"vgg16": lambda pre_trained_weights, include_top: VGG16(weights=pre_trained_weights, include_top=False)}
+
+
+def optimizers():
+    return {"sgd": lambda settings: SGD(settings.lr, settings.momentum, settings.decay, settings.nesterov),
+            "rmsprop": lambda settings: RMSprop(settings.lr) if settings.lr else RMSprop()}
 
 
 def base_model_for(settings):
-    model_name = settings["model_name"]
-    pre_trained_weights = settings["pre_trained_weights"]
-    include_top = settings["include_top"]
-    return models()[model_name](pre_trained_weights, include_top)
+    return models()[settings.model_name](settings.pre_trained_weights, settings.include_top)
+
+
+def optimizer_for(settings):
+    return optimizers()[settings.optimizer](settings)
 
 
 def compile_model(settings):
@@ -44,11 +53,10 @@ def compile_model(settings):
     predictions = Dense(1, activation='sigmoid')(x)
 
     model = Model(inputs=base_model.input, outputs=predictions)
-    all_trainable = settings["all_trainable"]
     for layer in base_model.layers:
-        layer.trainable = all_trainable
+        layer.trainable = settings.all_trainable
 
-    model.compile(optimizer='rmsprop', loss='binary_crossentropy',
+    model.compile(optimizer=optimizer_for(settings), loss='binary_crossentropy',
                   metrics=['accuracy', PerformanceMetrics.precision,
                            PerformanceMetrics.recall, PerformanceMetrics.fmeasure])
     return model
@@ -56,31 +64,25 @@ def compile_model(settings):
 
 def train_model(model, settings, train_images, train_labels, validation_images,
                 validation_labels, verbose=True):
-    model_name = settings["model_name"]
-    batch_size = settings["batch_size"]
-    epochs = settings["epochs"]
-
-    checkpoint_dir = ProjectPaths.checkpoint_dir_for(model_name, batch_size, epochs)
-    model_checkpoint_callback = ModelCheckpoint(checkpoint_dir, monitor='val_acc', verbose= verbose,
+    checkpoint_dir = ProjectPaths.checkpoint_dir_for(settings.model_name, settings.batch_size, settings.epochs)
+    model_checkpoint_callback = ModelCheckpoint(checkpoint_dir, monitor='val_acc', verbose=verbose,
                                                 save_weights_only=True,
-                                          save_best_only=True, mode='max', period=1)
+                                                save_best_only=True, mode='max', period=1)
 
-    log_dir = ProjectPaths.log_dir_for(model_name, batch_size, epochs, 0)
+    log_dir = ProjectPaths.log_dir_for(settings.model_name, settings.batch_size, settings.epochs, 0)
     tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=0,  write_graph=False, write_images=False)
 
-    model.fit(x=train_images, y=train_labels, epochs=epochs,
+    model.fit(x=train_images, y=train_labels, epochs=settings.epochs,
               callbacks=[model_checkpoint_callback, tensorboard_callback],
-              validation_data=(validation_images, validation_labels), batch_size=batch_size,
+              validation_data=(validation_images, validation_labels), batch_size=settings.batch_size,
               verbose=verbose)
 
 
 def evaluate_model(model, settings, train_images, train_labels, test_images, test_labels,
                    validation_images, validation_labels):
-    model_name = settings["model_name"]
-    batch_size = settings["batch_size"]
-    train_acc = round(model.evaluate(train_images, train_labels, batch_size)[1],4)
-    valid_acc = round(model.evaluate(validation_images, validation_labels, batch_size)[1],4)
-    test_acc = round(model.evaluate(test_images, test_labels, batch_size)[1],4)
+    train_acc = round(model.evaluate(train_images, train_labels, settings.batch_size)[1],4)
+    valid_acc = round(model.evaluate(validation_images, validation_labels, settings.batch_size)[1],4)
+    test_acc = round(model.evaluate(test_images, test_labels, settings.batch_size)[1],4)
 
     #confusion_matrix(true_labels, predicted_labels)
 
@@ -88,25 +90,28 @@ def evaluate_model(model, settings, train_images, train_labels, test_images, tes
     print("- Training accuracy:\t{}".format(train_acc))
     print("- Validation accuracy:\t{}".format(valid_acc))
     print("- Test accuracy:\t{}".format(test_acc))
-    return model_name, train_acc, test_acc, valid_acc
+    return settings.model_name, train_acc, test_acc, valid_acc
 
 
-run_settings = {"batch_size": 64,
-                "epochs": 1,
-                "model_name": "vgg16",
-                "pre_trained_weights": "imagenet",
-                "include_top": False,
-                "all_trainable": False
-                }
+def train_and_evaluate(run_settings_list):
+    model_evaluations = []
+    for run_settings in run_settings_list:
+        compiled_model = compile_model(run_settings)
+        train_model(compiled_model, run_settings, train_images, train_labels, valid_images, valid_labels)
+        model_evaluations.append(
+            evaluate_model(compiled_model, run_settings, train_images, train_labels, test_images, test_labels,
+                           valid_images, valid_labels))
+    return np.array(model_evaluations)
+
+
+run_settings = RunSettings(model_name="vgg16", pre_trained_weights="imagenet", include_top=False, all_trainable=False,
+                           batch_size=64, epochs=1, optimizer="rmsprop", lr=None, momentum=None, decay=None,
+                           nesterov=None)
+
 
 model_names = models().keys()
-model_evaluations = []
-for model_name in model_names:
-    compiled_model = compile_model(run_settings)
-    train_model(compiled_model, run_settings, train_images, train_labels, valid_images, valid_labels)
-    model_evaluations.append(evaluate_model(compiled_model, run_settings, train_images, train_labels, test_images, test_labels, valid_images, valid_labels))
+np_model_evaluations = train_and_evaluate([run_settings])
 
 evaluation_names = ["model_name", "train_acc", "test_acc", "valid_acc"]
-np_model_evaluations = np.array(model_evaluations)
 evaluations = pd.DataFrame(np_model_evaluations, index=model_names, columns=evaluation_names)
 print(evaluations.head())
