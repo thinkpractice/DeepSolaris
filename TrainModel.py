047a649945880686eb7e471397c528ed9136f1ab
tjadejong <tjadejong@thinkpractice.nl>
2018-08-21 16:29:19 +0200

    Evaluate confusion matrices at different cuttoff values.

--- TrainModel.py
+++ TrainModel.py
@@ -15,6 +15,9 @@ RunSettings = namedtuple("RunSettings", ["model_name", "pre_trained_weights", "i
                                          "dataset_name", "batch_size", "epochs", "optimizer", "lr",
                                          "momentum", "decay", "nesterov"])
 
+def run_name_for(settings):
+    return "{}_pr_{}_al_{}_{}_e_{}_bs_{}_op_{}".format(settings.model_name, settings.pre_trained_weights, settings.all_trained,
+                                   settings.dataset_name, settings.epochs, settings.batch_size, settings.optimizer)
 
 def optimizers():
     return {"sgd": lambda settings: SGD(settings.lr, settings.momentum, settings.decay, settings.nesterov),
@@ -33,7 +36,7 @@ def compile_model(model, settings):
 
 
 def train_model(model, settings, train_images, train_labels, validation_images, validation_labels, verbose=True):
-    file_in_checkpoint_dir = ProjectPaths.file_in_checkpoint_dir(settings.model_name, settings.batch_size,
+    file_in_checkpoint_dir = ProjectPaths.file_in_checkpoint_dir(run_name_for(settings), settings.batch_size,
                                                                  settings.epochs, "{}__{epoch:02d}_{val_acc:.2f}.hdf5"
                                                                  .format(settings.model_name))
 
@@ -53,14 +56,18 @@ def evaluate_model(model, settings, datasets):
     column_headers = list(settings._fields)
     column_values = list(settings)
 
+    cut_offs = [0.1 + 0.1 * i for i in range(8)]
     for dataset in datasets:
         column_headers.extend(["{}_{}".format(dataset.name, metric_name) for metric_name in model.metrics_names])
         column_values.extend(model.evaluate(dataset.images, dataset.labels, settings.batch_size))
 
-        column_headers.extend(["{}_{}".format(dataset.name, confusion_label) for confusion_label in ["tn", "fp", "fn",
-                                                                                                     "tp"]])
-        # Confusion matrix assumes sigmoid cut-off at 0.5, below is negative, above is positive
-        predicted_labels = [np.round(prediction) for prediction in model.predict(dataset.images)]
+        column_headers.extend(["{}_{}_{}".format(dataset.name, cut_off, confusion_label)
+                               for confusion_label in ["tn", "fp", "fn", "tp"]
+                               for cut_off in cut_offs])
+        # Calculates confusion matrices for different cut-off at values
+        predicted_labels = [prediction < cut_off
+                            for prediction in model.predict(dataset.images)
+                            for cut_off in cut_offs]
         column_values.extend(confusion_matrix(dataset.labels, predicted_labels).ravel())
 
     return column_headers, column_values
@@ -75,7 +82,7 @@ def train_and_evaluate(run_settings_list):
         train_model(compiled_model, run_settings, dataset[0].images, dataset[0].labels, dataset[2].images, dataset[2].labels)
 
         headers, values = evaluate_model(compiled_model, run_settings, dataset)
-        yield run_settings.model_name, headers, values
+        yield run_name_for(run_settings), headers, values
 
 
 def train_evaluate_and_log(csv_filename, run_settings_list):
@@ -100,9 +107,18 @@ def train_evaluate_and_log(csv_filename, run_settings_list):
 
 
 def load_run_settings(filename):
-    return [RunSettings(model_name="vgg16", pre_trained_weights="imagenet", include_top=False, all_trainable=False,
-                        dataset_name="AcMüDüHo", batch_size=64, epochs=1, optimizer="rmsprop", lr=None, momentum=None,
-                        decay=None, nesterov=None) for _ in range(1)]
+    return [RunSettings(model_name=model, pre_trained_weights=weights, include_top=False, all_trainable=all_trainable,
+                        dataset_name=dataset, batch_size=batch_size, epochs=epochs, optimizer="rmsprop",
+                        lr=None, momentum=None,
+                        decay=None, nesterov=None)
+            for model in ["vgg16", "vgg19", "xception", "resnet50"] #ModelFactory.available_base_models()
+            for dataset in Datasets.available_datasets()[:-1]
+            for weights in ["imagenet", ""]
+            for all_trainable in [False, True]
+            for epochs in [200]
+            for batch_size in [64]
+            ]
+
 
 
 def main(argv):
