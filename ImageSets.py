eb702f6da9e896d528ba5f3aeeb3db2d5aa7791b
Tim De Jong <tjadejong@thinkpractice.nl>
2019-01-10 14:32:35 +0100

    Started implementing ImageSets class that filters files according to some predefined rules.

---
+++ ImageSets.py
@@ -0,0 +1,44 @@
+import os
+
+
+class FileFilter(object):
+    def __init__(self, file_list, filter=""):
+        self.__file_list = file_list
+        self.__filter = filter
+
+    @property
+    def files(self):
+        return self.filter_files(self.__file_list, lambda filename: self.__filter in filename)
+
+    @property
+    def images(self):
+        return self.filter_files(self.files, lambda filename: "mask" not in filename)
+
+    @property
+    def masks(self):
+        return self.filter_files(self.files, lambda filename: "mask" in filename and "masked" not in filename)
+
+    @property
+    def masked(self):
+        return self.filter_files(self.files, lambda filename: "masked" in filename)
+
+    @staticmethod
+    def filter_files(file_paths, filter_function):
+        return sorted([filename for filename in file_paths if filter_function(filename)])
+
+
+class ImageSets(object):
+    def __init__(self, root_path):
+        self.__root_path = root_path
+
+    @property
+    def root_path(self):
+        return self.__root_path
+
+    @property
+    def rgb(self):
+        return FileFilter(os.listdir(self.root_path), "rgb")
+
+    @property
+    def ir(self):
+        return FileFilter(os.listdir(self.root_path), "ir")
---
+++ UnitTests/ImageSetsTests.py
@@ -0,0 +1,63 @@
+import unittest
+from ImageSets import ImageSets
+
+
+class ImageSetsTests(unittest.TestCase):
+    def test_rgb_returns_rgb_files(self):
+        self.assertEquals(["0000085b-6f4a-475a-b800-9ed7619e8a67_rgb_2013.tiff",
+                           "00001900-3f87-4a57-8e1a-8fbdf3127213_rgb_2013.tiff",
+                           "00023872-f873-4889-9b80-113677b9e86a_rgb_2013.tiff",
+                           "00024a4c-54fe-4d74-838b-20b8ee7f4777_rgb_2013.tiff",
+                           "00031d78-fd98-4371-ac7f-8f2ddf487021_rgb_2013.tiff",
+                           "00036fd4-14a1-49a4-ae79-feaa26847d61_rgb_2013.tiff"],
+                          ImageSets(r"TestData/2013").rgb.images)
+
+    def test_rgb_mask_returns_mask_files(self):
+        self.assertEquals(["0000085b-6f4a-475a-b800-9ed7619e8a67_rgb_2013_mask.tiff",
+                           "00001900-3f87-4a57-8e1a-8fbdf3127213_rgb_2013_mask.tiff",
+                           "00023872-f873-4889-9b80-113677b9e86a_rgb_2013_mask.tiff",
+                           "00024a4c-54fe-4d74-838b-20b8ee7f4777_rgb_2013_mask.tiff",
+                           "00031d78-fd98-4371-ac7f-8f2ddf487021_rgb_2013_mask.tiff",
+                           "00036fd4-14a1-49a4-ae79-feaa26847d61_rgb_2013_mask.tiff"],
+                          ImageSets(r"TestData/2013").rgb.masks)
+
+    def test_rgb_masked_returns_masked_files(self):
+        self.assertEquals(["0000085b-6f4a-475a-b800-9ed7619e8a67_rgb_2013_masked.tiff",
+                           "00001900-3f87-4a57-8e1a-8fbdf3127213_rgb_2013_masked.tiff",
+                           "00023872-f873-4889-9b80-113677b9e86a_rgb_2013_masked.tiff",
+                           "00024a4c-54fe-4d74-838b-20b8ee7f4777_rgb_2013_masked.tiff",
+                           "00031d78-fd98-4371-ac7f-8f2ddf487021_rgb_2013_masked.tiff",
+                           "00036fd4-14a1-49a4-ae79-feaa26847d61_rgb_2013_masked.tiff"],
+                          ImageSets(r"TestData/2013").rgb.masked)
+
+
+    def test_ir_returns_ir_files(self):
+        self.assertEquals(["0000085b-6f4a-475a-b800-9ed7619e8a67_ir_2013.tiff",
+                           "00001900-3f87-4a57-8e1a-8fbdf3127213_ir_2013.tiff",
+                           "00023872-f873-4889-9b80-113677b9e86a_ir_2013.tiff",
+                           "00024a4c-54fe-4d74-838b-20b8ee7f4777_ir_2013.tiff",
+                           "00031d78-fd98-4371-ac7f-8f2ddf487021_ir_2013.tiff",
+                           "00036fd4-14a1-49a4-ae79-feaa26847d61_ir_2013.tiff"],
+                          ImageSets(r"TestData/2013").ir.images)
+
+    def test_ir_mask_returns_mask_files(self):
+        self.assertEquals(["0000085b-6f4a-475a-b800-9ed7619e8a67_ir_2013_mask.tiff",
+                           "00001900-3f87-4a57-8e1a-8fbdf3127213_ir_2013_mask.tiff",
+                           "00023872-f873-4889-9b80-113677b9e86a_ir_2013_mask.tiff",
+                           "00024a4c-54fe-4d74-838b-20b8ee7f4777_ir_2013_mask.tiff",
+                           "00031d78-fd98-4371-ac7f-8f2ddf487021_ir_2013_mask.tiff",
+                           "00036fd4-14a1-49a4-ae79-feaa26847d61_ir_2013_mask.tiff"],
+                          ImageSets(r"TestData/2013").ir.masks)
+
+    def test_ir_masked_returns_masked_files(self):
+        self.assertEquals(["0000085b-6f4a-475a-b800-9ed7619e8a67_ir_2013_masked.tiff",
+                           "00001900-3f87-4a57-8e1a-8fbdf3127213_ir_2013_masked.tiff",
+                           "00023872-f873-4889-9b80-113677b9e86a_ir_2013_masked.tiff",
+                           "00024a4c-54fe-4d74-838b-20b8ee7f4777_ir_2013_masked.tiff",
+                           "00031d78-fd98-4371-ac7f-8f2ddf487021_ir_2013_masked.tiff",
+                           "00036fd4-14a1-49a4-ae79-feaa26847d61_ir_2013_masked.tiff"],
+                          ImageSets(r"TestData/2013").ir.masked)
+
+
+if __name__ == '__main__':
+    unittest.main()
