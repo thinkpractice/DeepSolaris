ea8a676eb429cea6c2f7eab96116ce2d264ad756
Tim De Jong <tjadejong@thinkpractice.nl>
2019-05-03 11:44:25 +0200

    Fixed some bugs and added some missing imports.

--- evaluate_vgg16.py
+++ evaluate_vgg16.py
@@ -1,18 +1,22 @@
 from sklearn.model_selection import RandomizedSearchCV
 from cbds.deeplearning import Project, ImageGenerator
 from cbds.deeplearning.models.vgg16 import vgg16_like_from_list
-from scipy.stats import randint as sp_randint 
 from scipy.stats import uniform
 from keras.layers import Dense, Dropout, Flatten
+from keras.models import Model
+from itertools import product
+
 
 def create_model(input_shape, layer_list):
-    vgg16_conv = vgg16_like_from_list(input_shape, layer_list)
-    x = Flatten()(vgg16_conv.output)
+    base_model = vgg16_like_from_list(input_shape, layer_list)
+    x = Flatten()(base_model.output)
     x = Dense(512, activation="relu")(x)
     x = Dropout(0.5)(x)
     predictions = Dense(1, activation="sigmoid")(x)
-    model_name = "vgg16_{}".format("_".join(["{}_{}".format(layer_type, layer_size) for layer_type, layer_size in layer_list))
+    model_name = "vgg16_{}_fc512_fc1".format("_".join(["{}_{}".format(layer_type, layer_size)
+                                                       for layer_type, layer_size in layer_list]))
     return model_name, Model(base_model.input, predictions)
+
 
 def create_models(input_shape):
     layer_depths = [64, 128, 256, 512, 512]
@@ -23,6 +27,7 @@ def create_models(input_shape):
             for layer_combination in layer_combinations
             ]
 
+
 def main():
     with Project(project_path=r"/media/megatron/Projects/DeepSolaris") as project:
         dataset = project.dataset("Heerlen-HR")
@@ -31,7 +36,8 @@ def main():
 
         train_dataset, test_dataset = dataset.split(test_size=0.2)
         train_generator = ImageGenerator(train_dataset)\
-                            .with_shuffle_data(True)\
+                            .with_shuffle_data(True) \
+                            .with_rescale(1./255)
                             # .with_rotation_range(45)\
                             # .with_width_shift_range(0.1)\
                             # .with_height_shift_range(0.1)\
@@ -39,7 +45,6 @@ def main():
                             # .with_zoom_range(0.1)\
                             # .with_channel_shift_range(0.1)\
                             # .with_horizontal_flip(0.1)\
-                            .with_rescale(1./255)
         test_generator = ImageGenerator(test_dataset)\
                             .with_rescale(1./255)
 
@@ -50,13 +55,16 @@ def main():
         for name, cnn_model in create_models(input_shape=image_shape):
             model = project.model(name)
             model.create_model(cnn_model)
-            keras_model = model.get_keras_classifier_model(epochs=100, batch_size=32, loss_function="binary_crossentropy", train_dataset=train_dataset, test_dataset=test_dataset)
+            keras_model = model.get_keras_classifier_model(epochs=100, batch_size=32,
+                                                           loss_function="binary_crossentropy",
+                                                           train_dataset=train_generator, test_dataset=test_generator)
 
             randomized_search = RandomizedSearchCV(keras_model, param_distributions={
                     "learn_rate": uniform(),
                     "momentum": [0.7, 0.8, 0.9],
                     "nesterov": [True, False]
                 })
+            randomized_search.fit(train_dataset.data, train_dataset.labels)
 
 
 if __name__ == "__main__":
