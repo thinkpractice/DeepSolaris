a277653b156fff1d498caf7526a5ae13be5ea517
Tim De Jong <tjadejong@thinkpractice.nl>
2019-05-03 11:56:37 +0200

    create_models now returns a generator instead of a list.

--- evaluate_vgg16.py
+++ evaluate_vgg16.py
@@ -5,6 +5,7 @@ from scipy.stats import uniform
 from keras.layers import Dense, Dropout, Flatten
 from keras.models import Model
 from itertools import product
+import os
 
 
 def create_model(input_shape, layer_list):
@@ -22,22 +23,24 @@ def create_models(input_shape):
     layer_depths = [64, 128, 256, 512, 512]
     layer_types = ["conv2", "conv3"]
     layer_combinations = [layer_combination for i in range(6) for layer_combination in product(layer_types, repeat=i)]
-    return [create_model(input_shape, [(layer_type, layer_depth)
+    return (create_model(input_shape, [(layer_type, layer_depth)
             for layer_type, layer_depth in zip(layer_combination, layer_depths)])
             for layer_combination in layer_combinations
-            ]
+            )
 
 
 def main():
-    with Project(project_path=r"/media/megatron/Projects/DeepSolaris") as project:
+    project_path = r"/media/megatron/Projects/DeepSolaris"
+    project_path = r"/media/tim/Data/Work/CBS/DeepSolaris"
+    with Project(project_path=project_path) as project:
         dataset = project.dataset("Heerlen-HR")
-        #dataset.import_numpy_dataset(r"/media/megatron/9827-B092/hr_2018_18m_all.npy", r"/media/megatron/9827-B092/hr_2018_18m_all_labels.npy")
-
+        #dataset.import_numpy_dataset(os.path.join(project_path, r"Images/hr_2018_18m_all.npy"),
+        #                             os.path.join(project_path, r"Images/hr_2018_18m_all_labels.npy"))
 
         train_dataset, test_dataset = dataset.split(test_size=0.2)
         train_generator = ImageGenerator(train_dataset)\
-                            .with_shuffle_data(True) \
-                            .with_rescale(1./255)
+            .with_shuffle_data(True)\
+            .with_rescale(1./255)
                             # .with_rotation_range(45)\
                             # .with_width_shift_range(0.1)\
                             # .with_height_shift_range(0.1)\
@@ -55,12 +58,12 @@ def main():
         for name, cnn_model in create_models(input_shape=image_shape):
             model = project.model(name)
             model.create_model(cnn_model)
-            keras_model = model.get_keras_classifier_model(epochs=100, batch_size=32,
+            keras_model = model.get_keras_classifier_model(epochs=30, batch_size=32,
                                                            loss_function="binary_crossentropy",
                                                            train_dataset=train_generator, test_dataset=test_generator)
 
             randomized_search = RandomizedSearchCV(keras_model, param_distributions={
-                    "learn_rate": uniform(),
+                    "lr": uniform(),
                     "momentum": [0.7, 0.8, 0.9],
                     "nesterov": [True, False]
                 })
