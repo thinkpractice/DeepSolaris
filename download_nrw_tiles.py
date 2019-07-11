46a6ee772298f5717803c8bad4d3d083fed3f0f3
Tim De Jong <tjadejong@thinkpractice.nl>
2019-07-11 10:16:31 +0200

    Added support for shapefiles

--- download_nrw_tiles.py
+++ download_nrw_tiles.py
@@ -27,7 +27,7 @@ def write_image(filename, image):
     with open(filename, 'wb') as image_file:
         image_file.write(image.read())
 
-def get_number_of_images(filename):
+def get_number_of_images_csv(filename):
     number_of_images = 0
     with open(filename, "r") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=";")
@@ -36,8 +36,16 @@ def get_number_of_images(filename):
 
     return number_of_images
 
-def get_locations_from_csv(filename, output_directory, image_format):
-    with open(, "r") as csv_file:
+def get_number_of_images_shp(filename):
+    with shapefile.Reader(filename) as shp:
+        return len(shp.shapeRecords())
+
+def get_number_of_images(filename):
+    return get_number_of_images_csv(filename) if filename.endswith(".csv") else get_number_of_images_shp(filename)
+
+
+def get_locations_from_csv(filename, output_directory, image_extension):
+    with open(filename, "r") as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=";")
         for row in csv_reader:
             longitude = float(row["x"])
@@ -45,15 +53,23 @@ def get_locations_from_csv(filename, output_directory, image_format):
             label = "positive" if row["label"].lower() == "true" else "negative"
             
             output_path = os.path.join(os.path.join(output_directory, label))
-            output_path = os.path.join(output_path, "{}.{}".format(row["object_id"], image_format.split("/")[-1]))
+            output_path = os.path.join(output_path, "{}.{}".format(row["object_id"], image_extension))
             yield output_path, longitude, latitude
 
-def get_locations_from_shapefile(filename, output_directory, image_format):
-    pass
+def get_locations_from_shapefile(filename, output_directory, image_extension):
+    with shapefile.Reader(filename) as shp:
+        for record in shp.shapeRecords():
+            object_id = record.record["LANUV_ID"]
+            point = record.shape.points
+            label = "positive"
+
+            output_path = os.path.join(os.path.join(output_directory, label))
+            output_path = os.path.join(output_path, "{}.{}".format(object_id, image_extension))
+            yield output_path, point[0][0], point[0][1] 
 
 
 def get_locations(filename, output_directory, image_format):
-    yield from get_locations_from_csv(filename, output_directory, image_format) if filename.endswith(".csv") else get_location_from_shapefile(filename)
+    yield from get_locations_from_csv(filename, output_directory, image_format) if filename.endswith(".csv") else get_locations_from_shapefile(filename, output_directory, image_format)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", required=True, help="The input csv file with the coordinates and annotations")
@@ -72,7 +88,9 @@ create_dir(os.path.join(args["output"], "negative"))
 number_of_images = get_number_of_images(args["input"])
 widgets = ["Classifying images: ", progressbar.Percentage(), " ", progressbar.Bar(), " ", progressbar.ETA()]
 pbar = progressbar.ProgressBar(maxval=number_of_images, widgets=widgets).start()
-for i, (output_path, longitude, latitude) in enumerate(get_locations(args["input"], args["output"], args["image_format"])):
+
+image_extension = args["image_format"].split("/")[-1]
+for i, (output_path, longitude, latitude) in enumerate(get_locations(args["input"], args["output"], image_extension)):
     tile_image = download_tile(args["wms_service"], args["layer"], longitude, latitude, args["width"], args["dop"], args["image_format"])
     write_image(output_path, tile_image)
     pbar.update(i)
