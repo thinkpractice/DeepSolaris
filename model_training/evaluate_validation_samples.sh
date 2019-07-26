d6df06539ebfee0d42b1a277c1cd6be0815a2bd4
Tim De Jong <tjadejong@thinkpractice.nl>
2019-07-26 20:46:15 +0200

    More work on shell script to see how large the training set should be.

--- model_training/evaluate_validation_samples.sh
+++ model_training/evaluate_validation_samples.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
-set project_path=$1
-set dataset=$2
-set test_set="$dataset_test"
-set validation_set="$dataset_val"
-set model_filename=$3
-set model_name=$4
+project_path=$1
+dataset=$2
+test_set="${dataset}_test"
+validation_set="${dataset}_val"
+model_filename=$3
+model_name=$4
+
+echo $project_path
 
 python split_train_test_validation.py -p $project_path -d $dataset
-python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.1 -m $model_filename -n $model_name
-python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.2 -m $model_filename -n $model_name
-python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.3 -m $model_filename -n $model_name
-python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.4 -m $model_filename -n $model_name
-python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.5 -m $model_filename -n $model_name
-python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.6 -m $model_filename -n $model_name
-python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.7 -m $model_filename -n $model_name
-python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.8 -m $model_filename -n $model_name
-python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.9 -m $model_filename -n $model_name
-python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 1.0 -m $model_filename -n $model_name
+python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.1 -e 20 #-m $model_filename -n $model_name
+python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.2 -e 20 #-m $model_filename -n $model_name
+python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.3 -e 20 #-m $model_filename -n $model_name
+python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.4 -e 20 #-m $model_filename -n $model_name
+python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.5 -e 20 #-m $model_filename -n $model_name
+python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.6 -e 20 #-m $model_filename -n $model_name
+python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.7 -e 20 #-m $model_filename -n $model_name
+python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.8 -e 20 #-m $model_filename -n $model_name
+python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 0.9 -e 20 #-m $model_filename -n $model_name
+python evaluate_validation_set_performance.py -p $project_path -d $dataset -t $test_set -v $validation_set -s 1.0 -e 20 #-m $model_filename -n $model_name
 
 
--- model_training/evaluate_validation_set_performance.py
+++ model_training/evaluate_validation_set_performance.py
@@ -1,9 +1,24 @@
 from cbds.deeplearning import Project, ImageGenerator
 from cbds.deeplearning.metrics import ClassificationReportCallback, ConfusionMatrixCallback, PlotRocCallback
 from cbds.deeplearning.settings import RMSPropSettings
-from keras.models import load_model
+from cbds.deeplearning.models import vgg16
+from keras.models import load_model, Model
+from keras.layers import Flatten, Dense
+from keras import regularizers
 import argparse
 import os
+
+
+def create_vgg16_model(input_shape, layer_index=7):
+    base_model = vgg16(input_shape, include_top=False, batch_normalization=False)
+    for layer in base_model.layers[:layer_index]:
+        layer.trainable = False
+    for layer in base_model.layers[layer_index:]:
+        layer.trainable = True
+    x = Flatten()(base_model.output)
+    predictions = Dense(1, activation="sigmoid", kernel_regularizer=regularizers.l2(0.01))(x)
+    model_name = "vgg16_full_fc1_aug_frozen_rms_prop_l2_0.01"
+    return model_name, Model(base_model.input, predictions)
 
 
 def main():
@@ -16,12 +31,13 @@ def main():
     parser.add_argument("-e", "--epochs", default=10, type=int, help="The number of epochs to train the model")
     parser.add_argument("-b", "--batch_size", default=32, type=int, help="The batch_size to train with")
     parser.add_argument("-l", "--learning_rate", default=1e-4, type=float, help="The learning rate to train with")
-    parser.add_argument("-m", "--model-filename", required=True, help="The model to evaluate")
-    parser.add_argument("-n", "--model-name", required=True, help="The name of the model to evaluate")
+    #parser.add_argument("-m", "--model-filename", required=True, help="The model to evaluate")
+    #parser.add_argument("-n", "--model-name", required=True, help="The name of the model to evaluate")
     args = vars(parser.parse_args())
 
     with Project(project_path=args["project_path"]) as project:
         dataset = project.dataset(args["training_set"])
+        dataset.data = dataset.data[:, :, :, ::-1]
         train_dataset = dataset.sample(sample_size=args["sample_size"], random_state=42)
         test_dataset = project.dataset(args["test_set"])
         validation_dataset = project.dataset(args["validation_set"])
@@ -45,8 +61,8 @@ def main():
                                 .with_rescale(1/255.)\
                                 .with_seed(84)
 
-        cnn_model = load_model(args["model_filename"])
-        _, model_name = os.path.split(args["model_filename"])
+        #model_name, cnn_model = load_model(args["model_filename"])
+        model_name, cnn_model = create_vgg16_model(input_shape=(187, 187, 3))
         model = project.model("{}_{}".format(model_name, str(args["sample_size"])))
         model.create_model(cnn_model)
         model.plot()
@@ -60,7 +76,7 @@ def main():
                 .with_class_weights(train_dataset.class_weights)\
                 .with_train_dataset(train_generator)\
                 .with_test_dataset(test_generator)\
-                .with_evaluation_dataset(test_dataset)\
+                .with_evaluation_dataset(test_generator)\
                 .with_evaluation_dataset(validation_generator) as run:
                 run.train()
                 run.evaluate()
