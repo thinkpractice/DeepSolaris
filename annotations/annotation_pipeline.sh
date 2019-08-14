def9991fecd1d004108fdd9eef65b4f6d6e50e12
Tim De Jong <tjadejong@thinkpractice.nl>
2019-08-14 10:29:27 +0200

    Completed driver shell script

--- annotations/annotation_pipeline.sh
+++ annotations/annotation_pipeline.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -u
 
 while getopts ":i:m:p:o:" opt; do
   case $opt in
@@ -11,18 +12,20 @@ while getopts ":i:m:p:o:" opt; do
     p) dataset_paths="$OPTARG"
     ;;
     o) output_dir="$OPTARG"
+    ;;
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
   esac
 done
 
-labeled_annotation_file=
-annotations_with_filename_file=
-filtered_annotations_file=
+base_name="$(basename $annotation_file)"
+labeled_annotation_file="${base_name}_labeled.csv" 
+annotations_with_filename_file="${output_dir}/${base_name}_with_filename.csv" 
+filtered_annotations_file="${output_dir}/${base_name}_filtered.csv"
 
 python label_annotations.py -i $annotation_file -o $labeled_annotation_file
-python add_filename_to_annotations.py -i $labeled_annotation_file -o $annotations_with_filename_file -p $dataset_paths
-python filter_annotations.py -i $annotation_with_filename_file -o $filtered_annotations_file
+python add_filenames_to_annotations.py -i $labeled_annotation_file -o $annotations_with_filename_file -p $dataset_paths
+python filter_annotations.py -i $annotations_with_filename_file -o $filtered_annotations_file
 python annotations_to_numpy.py -i $filtered_annotations_file -o $output_dir
 python evaluate_dataset.py -i $output_dir -m $model
 
