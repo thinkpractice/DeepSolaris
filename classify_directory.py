870c9dce6af4faf209e91c69273f73793cf5724e
Tim De Jong <tjadejong@thinkpractice.nl>
2019-08-16 18:02:28 +0200

    Make classify_directory work with one hot encodings

--- classify_directory.py
+++ classify_directory.py
@@ -6,11 +6,24 @@ import argparse
 import os
 import numpy as np
 
+
 def load_image(filename):
     image = load_img(filename, target_size=(187, 187))
     image = img_to_array(image)
     image = image[:,:,::-1]
     return preprocess_input(image)
+
+
+def predictions_for(predictions, one_hot=False): 
+    if one_hot:
+        return np.argmax(predictions, axis=1)
+    return predictions
+
+def get_class(prediction, one_hot):
+    if one_hot:
+        return prediction
+    return 1 if prediction >= args["cut_off"] else 0
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-m", "--model", required=True, help="Model to use for classification")
@@ -21,6 +34,7 @@ ap.add_argument("-c", "--cut-off", type=float, default=0.5, help="The cut-off to
 args = vars(ap.parse_args())
 
 model = load_model(args["model"])
+one_hot = model.get_layer(index=-1).output_shape[-1] > 1
 
 images = [os.path.join(args["dataset"], image) for image in os.listdir(args["dataset"]) if image.endswith(".tiff")]
 positives_dir = os.path.join(args["output"], "positives")
@@ -36,10 +50,10 @@ pbar = progressbar.ProgressBar(maxval=len(images), widgets=widgets).start()
 for i in range(0, len(images), args["batch_size"]):
     image_filenames = images[i:i + args["batch_size"]]
     image_batch = np.stack([load_image(filename) for filename in image_filenames])
-    predictions = model.predict(image_batch, batch_size=args["batch_size"])
+    predictions = predictions_for(model.predict(image_batch, batch_size=args["batch_size"]), one_hot)
     for filename, prediction, image in zip(image_filenames, predictions, image_batch):
         original = load_img(filename)
-        if prediction >= args["cut_off"]:
+        if get_prediction(prediction, one_hot) == 1:
             save_img(os.path.join(positives_dir, os.path.basename(filename)), original)
             continue
         save_img(os.path.join(negatives_dir, os.path.basename(filename)), original)
