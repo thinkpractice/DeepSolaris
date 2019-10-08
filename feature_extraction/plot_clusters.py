28599683600d90933e46a545f133c25d6904aaac
Tim De Jong <tjadejong@thinkpractice.nl>
2019-10-08 17:14:49 +0200

    Small update to plot_clusters

--- feature_extraction/plot_clusters.py
+++ feature_extraction/plot_clusters.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 
 for i in range(8):
     print("Cluster {}".format(i))
-    c = np.load("{}.npy".format(i))
+    cluster_images = np.load("{}.npy".format(i))
+    print("Shape: {}".format(cluster_images.shape))
     _, ax = plt.subplots(3, 3)
     i = 0
     for r in range(3):
-        for co in range(3):
-            ax[r, co].imshow(c[i,:])
+        for c in range(3):
+            image = cluster_images[i,:]
+            print(image.shape)
+            ax[r, c].imshow(image)
             i += 1
     plt.show()
 
